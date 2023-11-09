49d41674d20663709388f49a28e4fc57aae385cf
natalia-st <nastgm55@gmail.com>
2023-11-09 11:29:53 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import termsData from './terms';
+import { Container, Row, Col, Button, Stack, Navbar, Card } from 'react-bootstrap';
+;
+
+
+
+const terms = termsData;
+
+const App: React.FC = () => {
+  const [selectedTerms, setSelectedTerms] = useState(terms);
+  const [meaning, setMeaning] = useState<string>('');
+  const [isMatch, setIsMatch] = useState(false);
+  const [seconds, setSeconds] = useState(10);
+  const [firstClick, setFirstClick] = useState(false); 
+  const [counter, setCounter] = useState(0)
+
+  const matchWordToMeaning = (index: number) => {    
+    if (selectedTerms[index].meaning === meaning) {
+      setIsMatch(true);      
+      randomMeaning();
+      setFirstClick(true);
+      setCounter(counter + 1)
+      if (counter === 15) {              
+        window.alert('Youve won!! Play again!');
+        setCounter(0)        
+      }
+    } else {
+      setIsMatch(false);
+      setFirstClick(true);
+    }
+  };
+
+  const resetGrid = () => {
+    const shuffledTerms = [...terms];
+    shuffledTerms.sort(() => Math.random() - 0.5);
+    setSelectedTerms(shuffledTerms);
+    setCounter(0)
+  };
+
+  const randomMeaning = () => {    
+    const idx = Math.floor(Math.random() * terms.length);
+    const newMeaning = terms[idx].meaning;
+    setMeaning(newMeaning);
+    setSeconds(10);    
+  };
+  
+  useEffect(() => {
+    randomMeaning();       
+  }, [])
+  
+
+  useEffect(() => {       
+    const timerId = setInterval(() => {     
+        if (seconds === 0) {
+          setSeconds(10)
+          resetGrid();         
+        } else {
+          setSeconds((seconds) => seconds - 1); 
+        }            
+    }, 1000);
+    return () => clearInterval(timerId);
+  }, [seconds]); 
+
+
+const congrats = () => {  
+  if (firstClick && isMatch) {   
+    return <h3>Fantastic! KeepGoing!</h3>
+  } else if (firstClick && !isMatch) {
+    return <h3>Oops, try again</h3>
+  } else {
+    return null
+  }
+}
+
+
+  return (
+    <>  
+    <Container className='gameTitle'>
+      <h1>The Fancy Words Game</h1>
+    </Container>         
+    <Container className='mt-5 '>        
+      <Row className='firstRow'>
+        <Col >
+          <Stack gap={3} className='text-center'>
+          <div className="timeLeft p-1"><h3>{`Complete 16 points by matching the meaning to one of the fancy words in the grid. You have ${seconds} seconds before you lose your points and the words change order.`}</h3></div>
+          <div className="counter p-1"><h1>{counter}</h1></div>
+          <div className='meaningCard'>
+            <Card className="meaning">
+            <Card.Body><h2>{meaning}</h2></Card.Body>
+          </Card>
+          </div>                                 
+          </Stack> 
+          <Button variant="light" className='anotherButton' onClick={randomMeaning}>Try another definition</Button>      
+        </Col>
+        </Row>
+        <Row>
+        <Col className='secondRow'>
+        <div className="congrats">
+            <div >{congrats()}</div>
+        </div>
+        <div className="BoxGrid" >
+          {selectedTerms.map((term, index) => (
+            <div
+              key={index}
+              className='BoxItem'            
+              onClick={() => matchWordToMeaning(index)}
+            >
+              {term.word}
+            </div>
+          ))}
+        </div>         
+        </Col>     
+        </Row>
+    </Container>
+    </>
+  );
+};
+
+export default App;
---
+++ src/terms.js
@@ -0,0 +1,77 @@
+module.exports = [
+  {
+    word: 'Balatron',
+    meaning: 'A buffoon, who speaks a lot of nonsense and is self-indulgent',
+  },
+  {
+    word: 'Bedevil',
+    meaning: 'Cause great and continual trouble to something',
+  },
+  {
+    word: 'Bolus',
+    meaning: 'A pill or drug',
+  },
+  {
+    word: 'Churlish',
+    meaning: 'Lacking civility or graciousnes',
+  },
+  {
+    word: 'Confabulate',
+    meaning: 'To hold an informal discussion',
+  },
+  {
+    word: 'Dangersome',
+    meaning: 'Dangerous',
+  },
+  {
+    word: 'Dewdropper',
+    meaning: 'A slacker',
+  },
+  {
+    word: 'Encumber',
+    meaning: 'To weigh down or burden',
+  },
+  {
+    word: 'Epistle',
+    meaning: 'A formal or elegant letter',
+  },
+  {
+    word: 'Frippet',
+    meaning: 'A frivolous or showy young woman',
+  },
+  {
+    word: 'Inamorata',
+    meaning: 'A woman with whom one is in love or has intimate relations',
+  },
+  {
+    word: 'Irksome',
+    meaning: 'Annoying or tedious',
+  },
+  {
+    word: 'Juvenescence',
+    meaning: 'The state of being youthful',
+  },
+  {
+    word: 'Pablum	',
+    meaning: 'Bland intellectual sustenance',
+  },
+  {
+    word: 'Peccadillos',
+    meaning: 'A slight offence',
+  },
+  {
+    word: 'Spanandry',
+    meaning: 'The extreme scarcity of males',
+  }, 
+]		
+	
+
+// Those who've watched the TV show Schitt's Creek, are undoubtedly aware of 
+// Moira Rose's (Catherine O'Hara) extensive and unique vocabulary. Words like 
+// "balatron," "dewdropper," "frippet," "callipygian," "bedevil," "confabulate," "spanandry,"
+//  "bombilate," and "pettifogging" (all real English words!) are used by Moira Rose are might 
+//  seem difficult and confusing because we don't hear them enough.
+
+// Here’s a guide to the Moira Rose terminology on Schitt’s Creek. Moira's wordly game
+
+// https://ielts.co.nz/newzealand/prepare/article-50-most-difficult-english-words-synonyms-updated-2023
